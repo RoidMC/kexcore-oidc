855919aa0a3531c3a7c954b48982980f127dcd49
chencu5958 <82279230+chencu5958@users.noreply.github.com>
2026-05-22 16:40:15 +0800

    chore(pkg/strings): 删除已废弃的字符串工具包及其测试代码
    该工具包的Contains方法已提示使用标准库slices.Contains替代，移除冗余的自定义实现和测试文件

--- pkg/strings/strings.go
+++
@@ -1,9 +0,0 @@
-package strings
-
-import "slices"
-
-// Deprecated: Use standard library [slices.Contains] instead.
-func Contains(list []string, needle string) bool {
-	// TODO(v4): remove package.
-	return slices.Contains(list, needle)
-}
